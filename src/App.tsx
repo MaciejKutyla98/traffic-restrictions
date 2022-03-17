ea7e47d99520805d23cf85ee506b506100ff28f1
Maciej <maciej.kutyla9@gmail.com>
2022-03-17 16:27:49 +0100

    Fetched Api from gov website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {MapGenerator} from "./components/MapGenerator/MapGenerator";
+import {FetchData} from "./components/FetchData/FetchData";
 
 function App() {
     return (
         <div className='app'>
             <MapGenerator />
+            <FetchData />
         </div>
     );
 }
--- src/components/FetchData/FetchData.tsx
+++ src/components/FetchData/FetchData.tsx
@@ -1,3 +1,29 @@
+export const FetchData = () => {
+    const url = 'https://cors-anywhere.herokuapp.com/\n' +
+        'https://www.archiwum.gddkia.gov.pl/dane/zima_html/utrdane.xml'
+    const fetchData = () => {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        })
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const xml = parser.parseFromString(data, "application/xml");
+                console.log(xml);
+            })
+            .catch(console.error);
+    }
+
+
+    fetchData()
+    return (
+        <div></div>
+    );
+}
+
 export const geojson = {
     type: 'FeatureCollection',
     features: [
